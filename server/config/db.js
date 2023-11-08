5a3d1fe20dc3bb99549bff02791dbdbf7d87fcb3
Massoud Rahmani <Rahmanimassoud8@gmail.com>
2023-11-08 10:28:45 -0800

    finished project

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@
 /*eslint-disable no-undef*/
 const mongoose = require('mongoose')
 
-let connectionString = process.env.MONGODB;
+let connectionString = `mongodb+srv://Rahmani:${process.env.MONGODB}@mernmode3.zafhpkv.mongodb.net/mern?retryWrites=true&w=majority`
 console.log(connectionString);
 
 mongoose.connect(connectionString, {
